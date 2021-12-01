77006df898f5aba1c44dfff2b0333ce6a9155512
chee <chee@snoot.club>
2021-12-01 12:13:14 +0000

    add day 1

---
+++ 2021/day-1/part1.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/env perl
+$last;
+$incs = 0;
+
+foreach (<>) {
+	 if ($last and $last < $_) {
+		  $incs += 1;
+	 }
+	 $last = $_;
+}
+
+print($incs);
---
+++ 2021/day-1/part2.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/env perl
+use 5.34.0;
+use List::Util qw(sum);
+open my $filehandle, "<", $ARGV[0] or die;
+my @input = <$filehandle>;
+my $incs = 0;
+my $last;
+
+foreach (@input) {
+	state $index = 0;
+	if ($index > 2) {
+		my $window = sum(@input[($index - 2)..$index]) . "\n";
+		if ($window > $last) {
+			$incs += 1;
+		}
+		$last = $window;
+	}
+	$index += 1;
+}
+
+say $incs;
